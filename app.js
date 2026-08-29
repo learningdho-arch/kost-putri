ccaba7308ed3ee50a4b90bd5a81f2534c3aa16db
learningdho-arch <learningdho@gmail.com>
2026-08-29 19:30:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 console.log("APP JS TEST - V2");
 
 document.addEventListener("DOMContentLoaded", function () {
-console.log("DOM READY - APP JS BERHASIL");
+  console.log("DOM READY - APP JS BERHASIL");
 
-var loginPage = document.getElementById("loginPage");
-var appContainer = document.getElementById("appContainer");
+  var loginPage = document.getElementById("loginPage");
+  var appContainer = document.getElementById("appContainer");
 
-if (loginPage) {
-loginPage.style.display = "block";
-}
+  if (loginPage) {
+    loginPage.style.display = "block";
+  }
 
-if (appContainer) {
-appContainer.style.display = "none";
-}
+  if (appContainer) {
+    appContainer.style.display = "none";
+  }
 });
